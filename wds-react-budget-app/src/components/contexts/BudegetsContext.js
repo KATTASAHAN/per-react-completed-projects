571d1f074fe922b515aacae4fa747ec00ad93d3c
KATTASAHAN <kattasahan2234@gmail.com>
2022-10-15 17:18:46 +0530

    delete node modules

--- wds-react-budget-app/src/components/contexts/BudegetsContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BudegetsContext = React.createContext()
- 
-export function useBudgets() {}
-
-export const BudgetsProvider = ({ children }) => {
-  return children;
-};
